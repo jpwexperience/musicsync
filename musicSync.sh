35fe9a07fdda2cd171943bbca18dfbd873e48a94
John Williams <darumswizel@gmail.com>
2019-12-12 14:08:58 -0600

    updated script

--- musicSync.sh
+++ musicSync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
 #Replace stuff with <> with your setup
-COMMAND="rsync -avzh --progress --exclude-from exclude-list.txt <music-library>/* /media/<username>/<fiio-library>/"
+COMMAND="rsync -avzh --progress --exclude-from exclude-list.txt ~/Music/* /media/<username>/<fiio-library>/"
 echo $COMMAND
 eval $COMMAND
